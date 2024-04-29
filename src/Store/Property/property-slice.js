d1ca23f3f306418c3a7a85be565d9542df766308
AnandGanta <111570025+AnandGanta@users.noreply.github.com>
2024-04-29 17:24:36 +0530

    Add files via upload

---
+++ src/Store/Property/property-slice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const propertySlice = createSlice({
+    //Slice name
+    name:"property",
+    //intialstate
+    initialState: {
+        properties: [], 
+        totalProperties: 0,
+        searchParams: {},
+        error:null,
+        loading: false,
+
+    },
+
+    reducers:{
+        getRequest(state){
+            state.loading = true;
+        },
+        //for actions to update properties state with fetch data
+        getProperties(state,action){
+            state.properties = action.payload.data;
+            state.totalProperties=action.payload.all_properties;
+            state.loading = false;
+        },   
+
+        //action to search parameters
+        updateSearchParams: (state,action) => {
+            state.searchParams = Object.keys(action.payload).length === 0
+            ? {}
+            : {
+                ...state.searchParams,
+                ...action.payload,
+            }
+        },
+        //action to update error state
+        getErrors(state,action) {
+            state.error = action.payload
+        }
+    }
+})
+
+export const propertyAction = propertySlice.actions
+export default propertySlice
